baa9fae44d5879325214801a015c6dcd6d6ad209
Rahulhadpad <rahulhadpad05@gmail.com>
2025-07-26 11:47:02 +0530

    Delete vege directory

--- vege/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Recepie(models.Model):
-    recepie_name=models.CharField(max_length=100)
-    recepie_description=models.TextField()
-    recepie_image=models.ImageField(upload_to='recepie')
-    
